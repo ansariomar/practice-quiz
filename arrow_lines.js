8ee6941fc5d404562ab1bb43b74660c537acd7ec
ansariomar <omar.ansari@cca.edu>
2018-03-28 00:07:04 -0700

    Update arrow_lines.js

--- arrow_lines.js
+++ arrow_lines.js
@@ -10,8 +10,8 @@ function draw()
   
   
   for (var q=0; q<=100; q = q + 10) {
-    line(q+5,5,q+95,100);
-		line(q+5,195, q+95, 100);
+line(q+5,5,q+95,100);
+line(q+5,195, q+95, 100);
     
     
   }
