5f2787b2e2d4e75a08b63a68fcdbffc7893822bb
ansariomar <omar.ansari@cca.edu>
2018-03-27 21:00:51 -0700

    Create arrow_lines.js

---
+++ arrow_lines.js
@@ -0,0 +1,18 @@
+
+function setup() {
+  createCanvas(200, 200);
+}
+
+function draw()
+{
+  
+  background(255);
+  
+  
+  for (var q=0; q<=100; q = q + 10) {
+    line(q+5,5,q+95,100);
+		line(q+5,195, q+95, 100);
+    
+    
+  }
+}
